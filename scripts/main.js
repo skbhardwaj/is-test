1e901f019f9ad853a66ad52aa43acdc28c942e44
Shrikrishna Bhardwaj <shrikrishna.it@gmail.com>
2021-01-09 03:36:40 +0530

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,4 @@
 (function () {
-  if (!Element.prototype.matches) {
-    Element.prototype.matches =
-      Element.prototype.msMatchesSelector ||
-      Element.prototype.webkitMatchesSelector;
-  }
-
-  if (!Element.prototype.closest) {
-    Element.prototype.closest = function (s) {
-      var el = this;
-
-      do {
-        if (Element.prototype.matches.call(el, s)) return el;
-        el = el.parentElement || el.parentNode;
-      } while (el !== null && el.nodeType === 1);
-      return null;
-    };
-  }
-})();
-
-(function () {
   const NAV_DATA_URL = "/datastore/navdata.json";
   const CLASSES = {
     openCTA: "btn-open",
